1702f32f23c46431bda327554174c4136f028e01
uthacalthing <11409677+uthacalthing@users.noreply.github.com>
2018-01-03 21:50:03 +0000

    ADD Dockerfile and run.sh script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# if config file does not exist in data folder, copy the original one
+if [ ! -f /gunbot/config.js ]
+then
+  cp /gunbot/config.js.origin /gunbot-data/config.js
+fi
+
+# create data files if they don't exist
+touch /gunbot-data/db.sqlite;
+touch /gunbot-data/err.log;
+touch /gunbot-data/out.log;
+
+# run gunthy-gui
+./gunthy-gui-linx64;
